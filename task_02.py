7d98ca55b87934f6869528390b606cc47f5d2190
Lalani Werake <lalanikw@gmail.com>
2015-02-18 21:42:31 +0000

    task_02.py

---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A raw input convert data."""
+
+HI = raw_input('What is the answer to the life the universe and everything?')
+MY_INTEGER = int(HI)
+print 'HI: {}, MY_INTEGER: {}'
